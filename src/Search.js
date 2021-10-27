3a0a4079bf1de0a07993d31e9836e1c8fa804158
OAhonen <olli.p.ahonen@tuni.fi>
2021-10-27 13:16:27 +0300

    Add component which displays data from the fetch

---
+++ src/Search.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+function Search(props) {
+  let pr = props.result;
+  if (pr === undefined) {
+    return <div>hello</div>
+  } else {
+    return (
+      <div>
+      GTIN: {pr.gtin}<br/>
+      Name: {pr.name}<br/>
+      Weight: {pr.weight}<br/>
+      Energy: {pr.energy}<br/>
+      </div>
+    )
+  }
+}
+
+export default Search;
