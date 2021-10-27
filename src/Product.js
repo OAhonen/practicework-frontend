2283f315b062b04f12b6f7c06dec333b6c8cedc9
OAhonen <olli.p.ahonen@tuni.fi>
2021-10-27 12:00:37 +0300

    Add ability to find by gtin

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 function Product() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [gtinResult, setGtinResult] = useState([]);
 
   useEffect(() => {
     const url = 'http://localhost:8080/product/all';
@@ -17,12 +18,27 @@ function Product() {
       }
     };
 
+    const gtin = '6420256001547'
+    const url2 = 'http://localhost:8080/product/search/' + gtin
+
+    const findByGtin = async () => {
+      try {
+        const response = await fetch(url2);
+        const json = await response.json();
+        setGtinResult(json);
+        console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
     fetchData();
+    findByGtin();
 }, []);
 
   return (
     <div>
-      hello
+      {products[0].id}
     </div>
   );
 }
