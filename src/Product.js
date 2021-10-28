3c3ec6429ed2d5bd42ff361a66ea25e1caceb4c3
OAhonen <olli.p.ahonen@tuni.fi>
2021-10-28 17:29:43 +0300

    Add ability to delete product

--- src/Product.js
+++ src/Product.js
@@ -2,15 +2,26 @@ import React, { useEffect, useState } from 'react';
 
 function Product(props) {
   let pr = props.result;
+
+  const deleteProduct = async (event) => {
+    console.log(pr.id)
+    await fetch(`http://localhost:8080/product/delete/${pr.id}`, {
+      method: 'DELETE',
+    })
+    window.location.reload();
+  }
+
   if (pr === undefined) {
     return <div></div>
   } else {
     return (
       <div>
+      Id: {pr.id}<br/>
       GTIN: {pr.gtin}<br/>
       Name: {pr.name}<br/>
       Weight: {pr.weight}<br/>
       Energy: {pr.energy}<br/>
+      <p onClick={deleteProduct}>DELETE PRODUCT</p>
       </div>
     )
   }
