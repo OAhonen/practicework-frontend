b286aab3714f0223592b9e69240cdfd988ab101e
OAhonen <olli.p.ahonen@tuni.fi>
2021-10-28 11:05:51 +0300

    Remove unnecessary code from Product

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import AdvancedSearch from './AdvancedSearch';
 import Search from './Search';
 
 function Product() {
-  const [products, setProducts] = useState([]);
   const [gtinResult, setGtinResult] = useState([]);
   const [loading, isLoading] = useState(false);
 
@@ -19,34 +19,7 @@ function Product() {
       } catch (error) {
         console.log("error", error);
       }
-    };
-
-    const gtin = '6420256001547'
-    const url2 = 'http://localhost:8080/product/search/' + gtin
-
-    const findByGtin = async () => {
-      try {
-        const response = await fetch(url2);
-        const json = await response.json();
-        setGtinResult(json);
-        console.log(json);
-      } catch (error) {
-        console.log("error", error);
-      }
-    };
-
-    fetchData();
-    findByGtin();
-}, []);
-*/
-
-/*
-  const handleSubmit = ((event) => {
-    event.preventDefault();
-    console.log(gtin)
-    fetch('http://localhost:8080/product/search/' + gtin)
-      .then(response => response.json)
-      .then(json => setGtinResult(json));
+    }
   });
   */
 
@@ -72,6 +45,7 @@ function Product() {
         <input type="submit" value="Submit" />
       </form>
       {!loading ? null : <Search result={gtinResult[0]}></Search>}
+      <AdvancedSearch></AdvancedSearch>
     </div>
   );
 }
