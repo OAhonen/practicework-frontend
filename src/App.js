a9f703ea967cb7ae6c14ea728adfc9cc275e4105
OAhonen <olli.p.ahonen@tuni.fi>
2021-10-28 15:06:49 +0300

    Rename product and search for clarity

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Product from './Product';
 import { Route, Switch } from 'react-router';
 import AdvancedSearch from './AdvancedSearch';
+import Search from './Search';
 
 function App() {
   return (
     <Switch>
-      <Route path="/" component={Product} exact/>
+      <Route path="/" component={Search} exact/>
       <Route path="/advanced" component={AdvancedSearch} exact/>
     </Switch>
   );
--- src/Search.js
+++ src/Search.js
@@ -1,19 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import AdvancedSearch from './AdvancedSearch';
+import Navbar from './Navbar';
+import Product from './Product';
 
-function Search(props) {
-  let pr = props.result;
-  if (pr === undefined) {
-    return <div>hello</div>
-  } else {
-    return (
-      <div>
-      GTIN: {pr.gtin}<br/>
-      Name: {pr.name}<br/>
-      Weight: {pr.weight}<br/>
-      Energy: {pr.energy}<br/>
-      </div>
-    )
+function Search() {
+  const [gtinResult, setGtinResult] = useState([]);
+  const [loading, isLoading] = useState(false);
+
+  /*
+  useEffect(() => {
+    const url = 'http://localhost:8080/product/all';
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setProducts(json);
+        console.log(json[0].id)
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+  });
+  */
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const g = {gtin: data.get('gtin')};
+    const hr = await fetch(`http://localhost:8080/product/search/${g.gtin}`);
+    const json = await hr.json();
+    setGtinResult(json);
+    isLoading(true)
   }
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Gtin:
+          <input type="text"
+                id="gtin"
+                name="gtin"/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {!loading ? null : <Product result={gtinResult[0]}></Product>}
+    </div>
+  );
 }
 
 export default Search;
